b629604cc8d8c439f8f5d7d078fb1f2cb9ee4ddd
badogan <basri.dogan.71@gmail.com>
2020-04-13 23:51:07 +0100

    working solution

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React, { Component, Fragment } from 'react';
-import previousIcon from './assets/icons/left-icon.png';
-import nextIcon from './assets/icons/right_icon.png';
-import thumb1 from './assets/images/thumb/tea-light-thumb.jpeg';
-import thumb2 from './assets/images/thumb/white-light-thumb.jpeg';
-import thumb3 from './assets/images/thumb/pink-light-thumb.jpeg';
-import thumb4 from './assets/images/thumb/tea-light-thumb.jpeg';
-import image1 from './assets/images/tea-light.jpeg';
-import image2 from './assets/images/white-light.jpeg';
-import image3 from './assets/images/pink-light.jpeg';
-import image4 from './assets/images/tea-light.jpeg';
+import React, { Component, Fragment } from "react";
+import previousIcon from "./assets/icons/left-icon.png";
+import nextIcon from "./assets/icons/right_icon.png";
+import thumb1 from "./assets/images/thumb/tea-light-thumb.jpeg";
+import thumb2 from "./assets/images/thumb/white-light-thumb.jpeg";
+import thumb3 from "./assets/images/thumb/pink-light-thumb.jpeg";
+import thumb4 from "./assets/images/thumb/tea-light-thumb.jpeg";
+import image1 from "./assets/images/tea-light.jpeg";
+import image2 from "./assets/images/white-light.jpeg";
+import image3 from "./assets/images/pink-light.jpeg";
+import image4 from "./assets/images/tea-light.jpeg";
 
-import './App.css';
+import "./App.css";
 import Viewer from "./components/Viewer";
 import Thumbs from "./components/Thumbs";
-
+import { parse } from "querystring";
 
 const catalogs = [
   {
@@ -35,17 +35,16 @@ const catalogs = [
 ];
 
 class App extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      title: 'Catalog Viewer',
+      title: "Catalog Viewer",
       catalogs: [...catalogs],
-      currentIndex: -1,
-      catalogSelected: catalogs[3],
+      currentIndex: 0,
+      catalogSelected: catalogs[0],
       slideActive: false,
       slideTimer: null,
-      slideDuration: 3000,
+      slideDuration: 3000
     };
     this.selectedCatalog = this.selectedCatalog.bind(this);
     this.previousClick = this.previousClick.bind(this);
@@ -58,57 +57,109 @@ class App extends Component {
 
   selectedCatalog(index) {
     this.setState({
-      currentIndex:index
-    })
+      currentIndex: parseInt(index),
+      catalogSelected: this.state.catalogs[parseInt(index)]
+    });
   }
 
   previousClick() {
-
+    if (this.state.currentIndex !== 0) {
+      let newIndex = this.state.currentIndex - 1;
+      this.selectedCatalog(newIndex);
+    } else {
+      let newOrder = [...this.state.catalogs];
+      let temp = newOrder.pop();
+      newOrder.unshift(temp);
+      this.setState({ catalogs: newOrder }, () => this.selectedCatalog(0));
+    }
   }
 
   nextClick() {
-
+    if (this.state.currentIndex !== this.state.catalogs.length - 1) {
+      let newIndex = this.state.currentIndex + 1;
+      this.selectedCatalog(newIndex);
+      // }
+    } else {
+      let newOrder = [...this.state.catalogs];
+      let temp = newOrder.shift();
+      newOrder.push(temp);
+      this.setState({ catalogs: newOrder }, () =>
+        this.selectedCatalog(this.state.catalogs.length - 1)
+      );
+    }
   }
 
   slideChange(event) {
+    this.setState({ slideActive: !this.state.slideActive }, () =>
+      this.resetSlideTimer(this.state.slideActive)
+    );
+  }
 
+  startSlide(){
+    this.state.slideTimer = setInterval(this.nextClick,this.state.slideDuration)
+  }
+
+  stopSlide(){
+    clearInterval(this.state.slideTimer)
   }
 
   resetSlideTimer(isActive = false) {
-
+    isActive && this.startSlide()
+    !isActive && this.stopSlide()
   }
 
   onSlideChange() {
-
+    this.slideChange();
   }
 
   render() {
     return (
-        <Fragment>
-          <div className="title" data-testid="app-title"> {this.state.title} </div>
-          <div className="catalog-outer">
-            <div className="catalog-view">
-              <div className="text-center">
-                <div className="view-outter text-center">
-                  <Viewer catalog={this.state.catalogSelected.image}/>
-                </div>
+      <Fragment>
+        <div className="title" data-testid="app-title">
+          {" "}
+          {this.state.title}{" "}
+        </div>
+        <div className="catalog-outer">
+          <div className="catalog-view">
+            <div className="text-center">
+              <div className="view-outter text-center">
+                <Viewer catalog={this.state.catalogSelected.image} />
               </div>
-            </div>
-            <div className="catalog-items">
-              <div className="previous" onClick={this.previousClick} data-testid="prev-icon">
-                <img src={previousIcon}/>
-              </div>
-              <div className="next" onClick={this.nextClick} data-testid="next-icon">
-                <img src={nextIcon}/>
-              </div>
-              <Thumbs items={this.state.catalogs} currentIndex={this.state.currentIndex} selectedCatalog={this.selectedCatalog}/>
-            </div>
-
-            <div className="slide-input">
-              <input type="checkbox" onChange={this.slideChange} className="test" data-testid="slide"/> Slide
             </div>
           </div>
-        </Fragment>
+          <div className="catalog-items">
+            <div
+              className="previous"
+              onClick={this.previousClick}
+              data-testid="prev-icon"
+            >
+              <img src={previousIcon} />
+            </div>
+            <div
+              className="next"
+              onClick={this.nextClick}
+              data-testid="next-icon"
+            >
+              <img src={nextIcon} />
+            </div>
+            <Thumbs
+              items={this.state.catalogs}
+              currentIndex={this.state.currentIndex}
+              selectedCatalog={this.selectedCatalog}
+            />
+          </div>
+
+          <div className="slide-input">
+            <input
+              type="checkbox"
+              onChange={this.slideChange}
+              className="test"
+              data-testid="slide"
+            />{" "}
+            Slide
+          </div>
+        </div>
+      </Fragment>
     );
   }
 }
